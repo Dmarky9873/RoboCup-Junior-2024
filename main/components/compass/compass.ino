af387d122eb31e2f29aa1f0bb95f701b09c04a00
Jacky Zhang <jackychipersonal@gmail.com>
2024-01-08 16:45:43 -0500

    update

--- main/components/compass/compass.ino
+++ main/components/compass/compass.ino
@@ -14,7 +14,7 @@ void setup(void)
   if(!bno.begin())
   {
     /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR! 🤓🤓🤓"); 
     while(1);
   }
   
