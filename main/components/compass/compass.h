ce164de24ec87f26de4c6009b052efff267c8c3e
Kyle Andersen <kyleandersen279@gmail.com>
2024-01-09 09:26:11 -0500

    Minor Fixes
    Fixes to class structure
    
    Changed constructor declaration
    
    Formatting Issues

--- main/components/compass/compass.h
+++ main/components/compass/compass.h
@@ -4,11 +4,10 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
-class Compass : private Component
-{
+class Compass : private Component {
 
-public: 
-  float readCompass(void); 
+public:
+  float readCompass(void);
   void init();
   Compass() {
     init();
@@ -16,34 +15,32 @@ public:
 
 private:
   Adafruit_BNO055 bno;
-
 };
 
-void Compass::init() 
-{
+void Compass::init() {
   bno = Adafruit_BNO055(55);
   Serial.begin(9600);
-  Serial.println("Orientation Sensor Test"); Serial.println("");
-  
+  Serial.println("Orientation Sensor Test");
+  Serial.println("");
+
   /* Initialise the sensor */
-  if(!bno.begin())
-  {
+  if (!bno.begin()) {
     /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR! 🤓🤓🤓"); 
-    while(1);
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR! 🤓🤓🤓");
+    while (1)
+      ;
   }
-  
+
   delay(1000);
-    
+
   bno.setExtCrystalUse(true);
 }
 
-float readCompass(void)
-{
-  /* Get a new sensor event */ 
-  sensors_event_t event; 
+float readCompass(void) {
+  /* Get a new sensor event */
+  sensors_event_t event;
   bno.getEvent(&event);
-  
+
   /* Display the floating point data */
   Serial.print("X: ");
   Serial.print(event.orientation.x, 4);
@@ -52,6 +49,6 @@ float readCompass(void)
   Serial.print("\tZ: ");
   Serial.print(event.orientation.z, 4);
   Serial.println("");
-  
+
   delay(100);
 }
--- main/components/components.h
+++ main/components/components.h
@@ -4,51 +4,48 @@
 #include <string.h>
 #define UNKNOWN_PIN 0xFF
 
-class Component
-{
+class Component {
 public:
-    uint8_t getPinMode(uint8_t pin);
-    Component(int pin_numbers[], uint8_t modes[], int number_of_pins, String component_name);
+  uint8_t getPinMode(uint8_t pin);
+  Component(int pin_numbers[], uint8_t modes[], int number_of_pins, String component_name);
 
 private:
-    const int num_pins;
-    const String name;
-    const int *pin_nums;
-    const int *mds;
+  const int num_pins;
+  const String name;
+  const int *pin_nums;
+  const uint8_t *mds;
 };
 
-Component::Component(int pin_numbers[2], uint8_t modes[2], int number_of_pins, String component_name) {
-    Serial.print("Initializing ");
-    Serial.print(name);
-    Serial.println("...");
+Component::Component(int pin_numbers[], uint8_t modes[], int number_of_pins, String component_name)
+  : num_pins(number_of_pins), name(component_name), pin_nums(pin_numbers), mds(modes) {
+  Serial.print("Initializing ");
+  Serial.print(name);
+  Serial.println("...");
+  Serial.println();
+
+  for (int i = 0; i < num_pins; i++) {
+    Serial.print("Initializing pin ");
+    Serial.print(pin_nums[i]);
     Serial.println();
 
-    for(int i = 0; i < number_of_pins; i++) {
-      Serial.print("Initializing pin ");
-      Serial.print(pin_numbers[i]);
-      Serial.println();
+    pinMode(pin_nums[i], mds[i]);
 
-      pinMode(pin_numbers[i], modes[i]);
+    Serial.print("Pin ");
+    Serial.print(pin_nums[i]);
+    Serial.print(" to ");
 
-      Serial.print("Pin ");
-      Serial.print(pin_numbers[i]);
-      Serial.print(" to ");
-
-      if (getPinMode(pin_numbers[i]) == INPUT) {
-        Serial.println("INPUT");
-      } else if (getPinMode(pin_numbers[i]) == OUTPUT) {
-        Serial.println("OUTPUT");
-      } else {
-        Serial.println("UNKNOWN PIN");
-      }
-
+    if (getPinMode(pin_nums[i]) == INPUT) {
+      Serial.println("INPUT");
+    } else if (getPinMode(pin_nums[i]) == OUTPUT) {
+      Serial.println("OUTPUT");
+    } else {
+      Serial.println("UNKNOWN PIN");
     }
+  }
 }
 
-#define UNKNOWN_PIN 0xFF
 
-uint8_t Component::getPinMode(uint8_t pin)
-{
+uint8_t Component::getPinMode(uint8_t pin) {
   uint8_t bit = digitalPinToBitMask(pin);
   uint8_t port = digitalPinToPort(pin);
 
