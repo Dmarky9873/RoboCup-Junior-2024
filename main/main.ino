fc8179312f19cc46a842c1dbd07fe0ef807b9e17
Jacky Zhang <jackychipersonal@gmail.com>
2024-01-10 17:18:40 -0500

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,14 @@
-// #include "./components/components.h"
+#include "./components/components.h"
+#include "./components/compass/compass.h"
+
+int pins[] = { 1, 2 };
+uint8_t modes[] = { OUTPUT, OUTPUT };
+
+Compass compass(pins, modes, 2, "compass");
 
 void setup() {
   Serial.begin(9600);
   // Serial.println("Beginning initialization...");
-
-  // int pins[] = { 1, 2 };
-  // uint8_t modes[] = { OUTPUT, OUTPUT };
-
-  // Component x(pins, modes, 2, "motor");
 }
 
 void loop() {
