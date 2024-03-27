978ac13d02c8dc5a38a02e043a02cd653a49c0a3
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2024-03-27 13:26:31 -0400

    Changes

--- main/components/shift-register/shift-register.h
+++ main/components/shift-register/shift-register.h
@@ -1,87 +1,112 @@
-#include "../components.h"
-
-class Shift_Register : public Component {
+class Shift_Register {
 
   public: 
-  
     /**
-    * Reads the values stored in a set of daisy-chained shift registers.
-    * 
-    * @param numOfChips the number of chips daisy-chained.
-    *
-    * @param sizeOfChips the size of the chips.
-    *
-    * @returns a 2D integer array with dimentions [`numOfChips` x `sizeOfChips`] where each value is a value once stored on the corresponding chips address.
-    */
-    int** readChips();
+      * TODO: Comment this
+      */
+    Shift_Register(int ioSelect_, int clockPulse_, int dataPin_) {
+      ioSelect = ioSelect_;
+      clockPulse = clockPulse_;
+      dataPin = dataPin_;
+      Serial.println("Shift Register initialized.");
+    }
 
     /**
-    * Constructor, sets private variables to params.
-    *
-    * @param `pin_numbers[]` integer array of pin-numbers that relate to an instance of the component class.
-    * @param `modes[]` integer array where modes[n] is the mode for pin_numbers[n].
-    * @param `number_of_pins` number of elements in the modes[] and pin_numbers[] arrays.
-    * @param `component_name` name of this component as a string.
-    */
-    Shift_Register(int pin_numbers[], uint8_t modes[], int number_of_pins, String component_name) : 
-    Component(pin_numbers, modes, number_of_pins, component_name){
+      * Reads the values stored in a set of daisy-chained shift registers.
+      * 
+      * @param numOfChips the number of chips daisy-chained.
+      *
+      * @param sizeOfChips the size of the chips.
+      *
+      * @returns a 2D integer array with dimentions [`numOfChips` x `sizeOfChips`] where each value is a value once stored on the corresponding chips address.
+      */
+    int** readChips(int numOfChips, int sizeOfChips) {
+      int** chipValues = new int*[numOfChips];
+      for (int i = 0; i < numOfChips; ++i) {
+        chipValues[i] = new int[sizeOfChips];
+      }
+      
+      chipParallelIn();
+      for (int chip = 0; chip < numOfChips; chip++) {
+        chipValues[chip] = readChip(sizeOfChips);
+      }
 
-  };
+      return chipValues;
+    }
+
+    void printChips(int seconds_delay){
+      int** chipValues = readChips(NUM_CHIPS, CHIPSIZE);
+      for (int chip = 0; chip < NUM_CHIPS; chip++) {
+        Serial.print("Chip #");
+        Serial.print(chip + 1);
+        Serial.print(": [ ");
+        for (int addr = 0; addr < CHIPSIZE; addr++) {
+          Serial.print(chipValues[chip][addr]);
+          Serial.print(" ");
+        }
+        Serial.print("]");
+        Serial.println();
+      }
+      delay(seconds_delay);
+    }
 
   private:
-    int CHIPSIZE; // Number of bits of data that the chip can manage.
-
     /**
       * Sets the chip to accept parallel input or produce serial output.
       *
       * @param input_type type of input to set chip to. 0 for parallel input 1 for serial.
       */
-    void ioType(int io_type);
+    void ioType(int io_type) {
+      digitalWrite(ioSelect, io_type);
+    }
 
     /**
       * Pulses the clock.
       *
       * Writes the clock pulse to low, then high once, in rapid succession.
       */
-    void clkPulse();
+    void clkPulse(){
+      digitalWrite(clockPulse, LOW);
+      digitalWrite(clockPulse, HIGH);
+    }
 
     /**
-      * Shifts in parallel data to the chip.
+      * Reads all 8 bits of the serial output of a shift register.
+      *
+      * @param sizeOfChips the size of the chips being read
+      * 
+      * @return integer array of size `CHIPSIZE` where each int is a respective chip bit.
       */
-    void shiftIN();
+    int* readChip(int sizeOfChips) {
+      int* vals = new int[sizeOfChips];
+
+      for(int j = 0; j < sizeOfChips; j++) {
+        int val = digitalRead(dataPin);
+
+        vals[j] = val;
+
+        clkPulse();
+      }
+
+      return vals;
+    }
+
+    /**
+      * Shifts in parallel data to the shift register.
+      */
+    void chipParallelIn() {
+      ioType(0);
+      clkPulse();
+      ioType(1);
+    }
 
     // VARIABLES
 
+    int ioSelect;
+    int clockPulse;
+    int dataPin;
+
+    const int NUM_CHIPS = 2;
+    const int CHIPSIZE = 8;
+
 };
-
-
-void ioType(int io_type) {
-  digitalWrite(ioSelect, io_type);
-}
-
-void clkPulse() {
-  digitalWrite(clockPulse, LOW);
-  digitalWrite(clockPulse, HIGH);
-}
-
-void shiftIN() {
-  ioType(0);
-  clkPulse();
-  ioType(1);
-}
-
-int* readChip() {
-  shiftIN(); // Shifts in the parallel data.
-
-  int* vals = new int[CHIPSIZE]; // Creates new integer array of size `CHIPSIZE` in which we will store the values of the shift register.
-
-  for(int j = 0; j < CHIPSIZE; j++) {
-    int val = digitalRead(dataOut); // Reads the current register of the chip.
-
-    vals[j] = val; // Stores the value inside the array.
-
-    clkPulse(); // Pulses the clock to shift the register by one.
-  }
-
-  return vals;
-}
--- main/main.ino
+++ main/main.ino
@@ -1,23 +1,45 @@
 #include "./components/components.h"
 // #include "./components/motor/motor.h"
 #include "./components/compass/compass.h"
-// #include "./components/shift-register/shift-register.h"
+#include "./components/shift-register/shift-register.h"
 
-int pins[] = { 1, 2 };
-uint8_t modes[] = { OUTPUT, OUTPUT };
+/*
+ * Uncomment lines marked with "// *" to test compass indivitually
+ * Uncomment lines marked with "// **" to test shift registers/IR sensors individually
+ */
 
-Compass compass(pins, modes, 2, "compass");
+
+// int pins[] = { 1, 2 };// *
+// uint8_t modes[] = { OUTPUT, OUTPUT };// *
+
+
+
+// Compass compass(pins, modes, 2, "compass");// *
+
+
+/**
+  * Teensy Pins for Shift Register:
+  * clockPulse: pin 33
+  * ioSelect: pin 34
+  * dataPin: pin 32
+  */
+Shift_Register shiftRegister(34 /* ioSelect */, 33 /* clockPulse */, 32 /* dataPin */); // **
 
 void setup() {
   Serial.begin(9600);
 
-  // initialize compass
-  compass.init(); //initializes bno sensor
-  // pinMode(A0, INPUT);
+
+  // compass.init(); // *
 }
 
+int n = 0;
+
 void loop() {
-  Serial.print("Compass Reading: ");
-  Serial.println(compass.readCompass());
-  // Serial.println(analogRead(A0));
+  // Serial.print("Compass Reading: ");// *
+  // Serial.println(compass.readCompass());// *
+
+  Serial.print("Reading number ");
+  Serial.println(n);
+  shiftRegister.printChips(250);
+  n++;
 }
