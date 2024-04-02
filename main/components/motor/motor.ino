4ff3319aa26e98b6844c8cd71f835b9a839f5fda
Daniel Markusson <46487826+Dmarky9873@users.noreply.github.com>
2024-04-02 15:51:49 -0400

    Update motor.ino

--- main/components/motor/motor.ino
+++ main/components/motor/motor.ino
@@ -1,9 +1,125 @@
-#include "../components.h"
+#include <Adafruit_BNO055.h>
 
-void setup(){
+const unsigned int COMPASS_BUFF = 15;  // +/- 15
+
+Adafruit_BNO055 bno;
+
+struct Motor {
+  // Member variables
+private:
+  unsigned int s_pin;
+  unsigned int dir_pin;
+  unsigned int br_pin;
+
+public:
+
+  // Constructor (optional)
+  Motor(unsigned int pin_, unsigned int dir_pin_, unsigned int br_pin_)
+    : s_pin(pin_), dir_pin(dir_pin_), br_pin(br_pin_) {
+    pinMode(s_pin, OUTPUT);
+    pinMode(dir_pin, OUTPUT);
+    pinMode(br_pin, OUTPUT);
+  }
+
+  void spin(int s) {
+    digitalWrite(br_pin, HIGH);
+    if (s < 0) {
+      analogWrite(s_pin, s * -1);
+      digitalWrite(dir_pin, HIGH);
+    } else {
+      analogWrite(s_pin, s);
+      digitalWrite(dir_pin, LOW);
+    }
+  }
+
+  void brake() {
+    analogWrite(s_pin, 0);
+    digitalWrite(br_pin, LOW);
+  }
+};
+
+
+Motor motor_0(3 /* Speed pin */, 4 /* Direction pin */, 2 /* Brake pin */);
+Motor motor_1(6 /* Speed pin */, 7 /* Direction pin */, 5 /* Brake pin */);
+Motor motor_2(9 /* Speed pin */, 10 /* Direction pin */, 8 /* Brake pin */);
+Motor motor_3(12 /* Speed pin */, 13 /* Direction pin */, 11 /* Brake pin */);
+
+Motor motors[4] = { motor_0, motor_1, motor_2, motor_3 };
+
+
+void setup() {
+  Serial.begin(9600);
+
+  initialize();
+}
+
+void loop() {
+  if (is_between(COMPASS_BUFF*-1, COMPASS_BUFF, readCompass())){
+    brake();
+  } else if (readCompass() > 0){
+    rotate(210);
+  } else {
+    rotate(-210);
+  }
 
 }
 
-void loop(){
 
+
+
+boolean is_between(int lower, int upper, int x){
+  if (lower < x && x < upper){
+    return true;
+  }
+  return false;
+}
+
+void rotate(int speed) {
+  for (int i = 0; i < 4; i++) {
+    motors[i].spin(speed);
+  }
+}
+
+void brake() {
+  for (int i = 0; i < 4; i++) {
+    motors[i].brake();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+void initialize() {
+  bno = Adafruit_BNO055(55);
+
+  /* Initialise the sensor */
+  if (!bno.begin()) {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR! 🤓🤓🤓");
+    while (1)
+      ;
+  }
+
+
+  bno.setExtCrystalUse(true);
+}
+
+float readCompass() {
+  /* Get a new sensor event */
+  sensors_event_t event;
+  bno.getEvent(&event);
+
+  int angle = event.orientation.x;
+
+  if (angle > 180){
+    return angle - 360;
+  }
+  return angle;
 }
