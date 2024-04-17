dd3a13a7389867380557655c5b4e6f2c11d2930d
Kyle Andersen <kyleandersen279@gmail.com>
2024-04-17 18:24:10 -0400

    Updated Cameras

--- main/components/camera/camera.h
+++ main/components/camera/camera.h
@@ -4,14 +4,12 @@
 struct Camera {
   Pixy2 pixy;
   int32_t centerX;
-  int32_t centerY;
   int32_t middleThreshold;
 
   Camera() {
     pixy.init();
-    centerX = pixy.frameWidth / 2;
-    centerY = pixy.frameHeight / 2;
-    middleThreshold = 100;
+    centerX = 155;     
+    middleThreshold = 50;  
   }
 
   String getObjectDirection() {
@@ -20,19 +18,42 @@ struct Camera {
 
     if (blocks) {
       int32_t midX = pixy.ccc.blocks[0].m_x;
-      int32_t midY = pixy.ccc.blocks[0].m_y;
 
-      if (midX > centerX - middleThreshold && midX < centerX + middleThreshold && midY > centerY - middleThreshold && midY < centerY + middleThreshold) {
-        return "In line with net";
+      if (midX > centerX - middleThreshold && midX < centerX + middleThreshold) {
+        return "CENTER";  // the camera can see the net directly in front of it
       } else {
         if (midX < centerX) {
-          return "Net on left side";
+          return "LEFT";
         } else {
-          return "Net on right side";
+          return "RIGHT";
         }
       }
     } else {
-      return "Net is Not Detected";
+      return "NONE";
+    }
+  }
+
+  int32_t getDistanceToCenter() {
+    int32_t blocks;
+    blocks = pixy.ccc.getBlocks();
+
+    if (blocks) {
+      int32_t midX = pixy.ccc.blocks[0].m_x;
+      int32_t distanceToThreshold = abs(centerX - midX) - middleThreshold;
+      return distanceToThreshold;  // Distance from object to threshold
+    } else {
+      return -1;  // Return a negative value to indicate no object detected
+    }
+  }
+
+  int32_t getRotationDistance() {
+    int32_t distanceToCenter = getDistanceToCenter();
+    if (distanceToCenter >= 0) {
+      // Calculate how much the robot needs to rotate to align with the threshold
+      int32_t rotationDistance = middleThreshold - distanceToCenter;
+      return rotationDistance;
+    } else {
+      return -1;  // No object detected, return a negative value
     }
   }
 };
